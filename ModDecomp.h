d580d5aa04d66cb3e0f8959c0da1acef1709eadb
koshelev <truemacintoshchannel@gmail.com>
2023-09-27 18:07:56 -0400

    Add file support

--- ModDecomp.h
+++ ModDecomp.h
@@ -34,6 +34,8 @@ struct vertex_prop {
 class ModDecomp {
 public:
   Graph readFromString(std::string inFile, bool directed);
+  Graph readFromStringDimacs(const std::string& input, bool directed);
+  Graph readFromFileDimacs(const std::string& filepath, bool directed);
   Tree decompose(Graph& g);
   Tree decompose_components(Graph& g);
   Tree calcModDecomp(const std::vector<Vertex>& factorization, Graph& graph);
